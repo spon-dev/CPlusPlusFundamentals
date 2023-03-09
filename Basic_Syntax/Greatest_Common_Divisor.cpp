fbc60ec41ba5ca3e090074a063d36a020f2ecf72
spon-dev <127289868+spon-dev@users.noreply.github.com>
2023-03-09 13:43:35 +0100

    Update Greatest_Common_Divisor.cpp

--- Basic_Syntax/Greatest_Common_Divisor.cpp
+++ Basic_Syntax/Greatest_Common_Divisor.cpp
@@ -4,16 +4,9 @@ using namespace std;
 int main()
 {
 
-
-
-
     int a, b, temp;
 
-
     cin >> a >> b;
-
-
-
 
     if (a < b)
 
@@ -25,35 +18,18 @@ int main()
 
         b = temp;
 
-
-
-
     }
-
-
 
     while (b > 0)
     {
-
 
         temp = b;
         b = a % b;
         a = temp;
 
-
     }
 
-
-
-
-
-
-
-
-
     cout << a << endl;
-
-
 
     return 0;
 }

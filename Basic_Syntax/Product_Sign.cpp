232a276ba2de5edeb2c07568e189ffaf6b9e3410
spon-dev <127289868+spon-dev@users.noreply.github.com>
2023-03-11 12:29:47 +0200

    Update Product_Sign.cpp

--- Basic_Syntax/Product_Sign.cpp
+++ Basic_Syntax/Product_Sign.cpp
@@ -1,14 +1,51 @@
 #include <iostream>
 using namespace std;
 
+bool isPositive(double a)
+{
+
+	return a > 0;
+
+}
+
 int main()
 {
-	int a, b, c;
+	double a, b, c;
 	cin >> a >> b >> c;
 
-	if (a*b*c >= 0)
+
+	if (a == 0 || b == 0 || c == 0)
 	{
 		cout << "+";
+
+		return 0;
+	}
+
+	/*
+	bool isPositiveA = a > 0;
+
+	bool isPositiveB = b > 0;
+
+	
+
+	bool output = (isPositiveA == isPositiveB);
+
+	isPositiveB = c > 0;
+
+	output = (output == isPositiveB);
+
+	*/
+
+	bool output = (isPositive(a) == isPositive(b));
+
+	output = (output == isPositive(c));
+
+
+	if (output == true)
+	{
+
+		cout << "+";
+
 	}
 
 	else

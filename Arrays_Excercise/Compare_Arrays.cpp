fe34fb9068048d31cb235d737e5d32c84dd9c94f
spon-dev <127289868+spon-dev@users.noreply.github.com>
2023-04-21 22:25:52 +0300

    Update Compare_Arrays.cpp

--- Arrays_Excercise/Compare_Arrays.cpp
+++ Arrays_Excercise/Compare_Arrays.cpp
@@ -1,23 +1,57 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+const int maxArraySize = 100;
 
+void enterArray(int arr[], int& arrSize)
+{
+	cin >> arrSize;
 
+	if (arrSize > maxArraySize)
+	{
+		cout << "Buffer too small. Needed: " << arrSize << endl;
+		exit(1);
+	}
 
+	for (size_t c = 0; c < arrSize; c++)
+	{
+		cin >> arr[c];
+	}
+}
+
+bool areEqual(int arr1[], int length1, int arr2[], int length2)
+{
+	if (length1 != length2)
+		return false;
+
+	for (size_t counter = 0; counter < length1; counter++)
+	{
+		if (arr1[counter]  != arr2[counter])
+		{
+			return false;
+		}
+		
+	}
+	return true;
+}
 
 int main()
 {
-	size_t counts[10] = { 0 };
-	int inputSize;
-	cin >> inputSize;
+	int arr1[maxArraySize], arr2[maxArraySize];
+	int arr1Size, arr2Size;
 
-	for (size_t c = 0; c < inputSize; c++)
+	enterArray(arr1, arr1Size);
+	enterArray(arr2, arr2Size);
+
+	if (areEqual(arr1, arr1Size, arr2, arr2Size))
 	{
-		int curNumber;
-		cin >> curNumber;
-
-		counts[curNumber]++;
+		cout << "equal" << endl;
 	}
-
+	else
+	{
+		cout << "Not equal" << endl;
+	}
+		
 
 
 	return 0;

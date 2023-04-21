368c8cc5eff97a9baf79d8622601b9e8485a6770
spon-dev <127289868+spon-dev@users.noreply.github.com>
2023-04-21 21:15:01 +0300

    Update Operation.cpp

--- Functions_Exercises/Operation.cpp
+++ Functions_Exercises/Operation.cpp
@@ -1,20 +1,62 @@
-// Operation.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+void isAddition(int a, int b)
+{
+	cout << a + b << endl;
+}
+
+void isSubtraction(int a, int b)
+{
+	cout << a - b << endl;
+}
+
+void isMultiplication(int a, int b)
+{
+	cout << a * b << endl;
+}
+
+void isDivision(int a, int b)
+{
+	if (b == 0)
+	{
+		cout << "Can't divide by zero." << endl;
+	}
+	else
+	{
+		cout << a / b << endl;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int a, b;
+	char operation;
+
+	cin >> a >> b;
+	cin >> operation;
+
+	
+	if (operation == '+' )
+	{
+		isAddition(a, b);
+	}
+	else if (operation == '-')
+	{
+		isSubtraction(a, b);
+	}
+	else if (operation == '*')
+	{
+		isMultiplication(a, b);
+	}
+	else if (operation == '/')
+	{
+
+		isDivision(a, b);
+	}
+	
+
+	return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
